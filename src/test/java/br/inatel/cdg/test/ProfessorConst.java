985400602700ef497aae4e49ab39e55c35c2e56f
Igor Luiz Rodrigues <89806466+igu1nho@users.noreply.github.com>
2023-03-27 14:33:58 -0300

    Update ProfessorConst.java

--- src/test/java/br/inatel/cdg/test/ProfessorConst.java
+++ src/test/java/br/inatel/cdg/test/ProfessorConst.java
@@ -6,77 +6,77 @@ public class ProfessorConst {
             "{ \"nomeDoProfessor\": \"Chris Lima\", \n " +
                     "\"horarioDeAtendimento\": \"sexta-feira, as 17:30\", \n " +
                     "\"periodo\": \"noturno\" , \n " +
-                    "\"sala\": 18, \n " +
-                    "\"predio\": 4}";
+                    "\"sala\":18, \n " +
+                    "\"predio\": [\"4, \"]}";
 
     public static String Vitor =
             "{ \"nomeDoProfessor\": \"Vitor Alexandre\", \n " +
                     "\"horarioDeAtendimento\": \"quarta-feira, as 17:30\", \n " +
                     "\"periodo\": \"noturno\" , \n " +
-                    "\"sala\": 22 , \n " +
-                    "\"predio\": 6}";
+                    "\"sala\":22, \n " +
+                    "\"predio\": [\"6, \"]}";
 
     public static String Renan =
             "{ \"nomeDoProfessor\": \"Renan Duque\", \n " +
                     "\"horarioDeAtendimento\": \"segunda-feira, as 13:30\", \n " +
                     "\"periodo\": \"integral\" , \n " +
-                    "\"sala\": 15 , \n " +
-                    "\"predio\": 3 }";
+                    "\"sala\":15 , \n " +
+                    "\"predio\": [\"3, \"]}";
 
     public static String Renzo =
             "{ \"nomeDoProfessor\": \"Renzo Paranaiba\", \n " +
                     "\"horarioDeAtendimento\": \"quarta-feira, as 17:30\", \n " +
                     "\"periodo\": \"noturno\" , \n " +
-                    "\"sala\": 18 , \n " +
-                    "\"predio\": 4 }";
+                    "\"sala\":18 , \n " +
+                    "\"predio\": [\"4, \"]}";
 
     public static String Marcelo =
             "{ \"nomeDoProfessor\": \"Marcelo Vinicius\", \n " +
                     "\"horarioDeAtendimento\": \"terca-feira, as 19:30\", \n " +
                     "\"periodo\": \"noturno\" , \n " +
-                    "\"sala\": 19 , \n " +
-                    "\"predio\": 4 }";
+                    "\"sala\":19 , \n " +
+                    "\"predio\": [\"4, \"]}";
 
     public static String Igor =
             "{ \"nomeDoProfessor\": \"Igor Luiz\", \n " +
                     "\"horarioDeAtendimento\": \"quinta-feira, as 10:00\", \n " +
                     "\"periodo\": \"integral\" , \n " +
-                    "\"sala\": 10 , \n " +
-                    "\"predio\": 2 }";
+                    "\"sala\":10 , \n " +
+                    "\"predio\": [\"2, \"]}";
 
     public static String Bruno =
             "{ \"nomeDoProfessor\": \"Bruno Oliveira\", \n " +
                     "\"horarioDeAtendimento\": \"sexta-feira, as 8:00\", \n " +
                     "\"periodo\": \"integral\" , \n " +
-                    "\"sala\": 3 , \n " +
-                    "\"predio\": 1 }";
+                    "\"sala\":3 , \n " +
+                    "\"predio\": [\"1, \"]}";
 
     public static String Leandro =
             "{ \"nomeDoProfessor\": \"Leandro Mucciaroni\", \n " +
                     "\"horarioDeAtendimento\": \"quinta-feira, as 10:00\", \n " +
                     "\"periodo\": \"integral\" , \n " +
-                    "\"sala\": 3 , \n " +
-                    "\"predio\": 1 }";
+                    "\"sala\":3 , \n " +
+                    "\"predio\": [\"1, \"]}";
 
     public static String Soned =
             "{ \"nomeDoProfessor\": \"Edson Josias\", \n " +
                     "\"horarioDeAtendimento\": \"segunda-feira, as 19:30\", \n " +
                     "\"periodo\": \"noturno\" , \n " +
-                    "\"sala\": 14 , \n " +
-                    "\"predio\": 3 }";
+                    "\"sala\":14 , \n " +
+                    "\"predio\": [\"3, \"]}";
 
     public static String Luis =
             "{ \"nomeDoProfessor\": \"Luis Felipe\", \n " +
                     "\"horarioDeAtendimento\": \"terca-feira, as 17:30\", \n " +
                     "\"periodo\": \"noturno\" , \n " +
-                    "\"sala\": 7 , \n " +
-                    "\"predio\": 2 }";
+                    "\"sala\":7 , \n " +
+                    "\"predio\": [\"2, \"]}";
 
     public static String INEXISTENTE =
             "{ \"nomeDoProfessor\": \"Inexistente\", \n " +
                     "\"horarioDeAtendimento\": \"Inexistente\", \n " +
                     "\"periodo\": \"Inexistente\" , \n " +
-                    "\"sala\": 0 , \n " +
-                    "\"predio\": 0 }";
+                    "\"sala\":0 , \n " +
+                    "\"predio\": [\"Inexistente\"]}";
 
 }
